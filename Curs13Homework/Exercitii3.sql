3639b574b949eee911cf45e9b4118e98c7467f9a
SimonaCalentir79 <simona.calentir@gmail.com>
2019-01-17 20:34:22 +0200

    exercitii

--- Curs13Homework/Exercitii3.sql
+++ Curs13Homework/Exercitii3.sql
@@ -1,29 +1,29 @@
 
---Ex.1: Names of all libraries from Iasi
+/*Ex.1: Names of all libraries from Iasi*/
 select NameOfLibrary, City from LibraryNomenclator ln left join AddressNomenclator an on ln.AddressID=an.AddressID where City='Iasi';
 
---Ex.2: Names of all members from ‘Biblioteca Centrala Mihai Eminescu’
+/*Ex.2: Names of all members from ‘Biblioteca Centrala Mihai Eminescu’*/
 select FirstName, LastName from Member where MemberID in 
 	(select MemberID from LibraryMember where LibraryID in
 	(select LibraryID from LibraryNomenclator where NameOfLibrary='Biblioteca Centrala Mihai Eminescu') );
 
---Ex.3: Names of all fantasy books from letter ‘P’ (Title begins with ‘P’) in ‘Biblioteca Centrala Mihai Eminescu’
+/*Ex.3: Names of all fantasy books from letter ‘P’ (Title begins with ‘P’) in ‘Biblioteca Centrala Mihai Eminescu’*/
 select Title from Book where BookID in
 	(select BookID from BookCategory bc inner join Category c on bc.CategoryID=c.CategoryID where c.NameOfCategory='Fantasy');
 
---Ex.4: Names of all books borrowed and not returned by Daniel Ionescu from ‘Biblioteca Centrala Mihai Eminescu’
+/*Ex.4: Names of all books borrowed and not returned by Daniel Ionescu from ‘Biblioteca Centrala Mihai Eminescu’*/
 select Title from Book b inner join Request r on b.BookID=r.BookID 
 	where b.BookID in (select BookID from Request where DateReturned is null) 
 	and r.MemberID in (select MemberID from Member where FirstName='Daniel' and LastName='Ionescu') 
 	and r.LibraryID in (select LibraryID from LibraryNomenclator where NameOfLibrary='Biblioteca Centrala Mihai Eminescu');
 
---Ex.5: Category name and number of books from each category in ‘Biblioteca Centrala Mihai Eminescu’
+/*Ex.5: Category name and number of books from each category in ‘Biblioteca Centrala Mihai Eminescu’*/
 select NameOfCategory, count (BookID) as NumberOfBooks 
 	from BookCategory bc 
 		inner join Category c on bc.CategoryID=c.CategoryID 
 	group by c.NameOfCategory;
 
---Ex.6: Name author of the most expensive book borrowed by Mihai Popescu from ‘Biblioteca Centrala Mihai Eminescu’
+/*Ex.6: Name author of the most expensive book borrowed by Mihai Popescu from ‘Biblioteca Centrala Mihai Eminescu’*/
 select MAX(Price)/*, Title, FirstName, LastName*/
 	from BookAuthor ba 
 		inner join Book b on ba.BookID=b.BookID
@@ -34,7 +34,7 @@ select MAX(Price)/*, Title, FirstName, LastName*/
 						and l.NameOfLibrary='Biblioteca Centrala Mihai Eminescu');
 	/*group by Title, FirstName, LastName;*/
 
---Ex.7: Total number of copies from ‘Fluturi’ in Iasi
+/*Ex.7: Total number of copies from ‘Fluturi’ in Iasi*/
 select Title, SUM(Quantity) as TotalCopiesInIasi
 	from BookLibrary bl
 		inner join Book b on bl.BookID=b.BookID
